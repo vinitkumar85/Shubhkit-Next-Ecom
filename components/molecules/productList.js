7fe1a4da31eb6620db86f9070e16281bc1da2ed5
Vinit Kumar <vinit.k.85@gmail.com>
2020-07-27 14:18:10 +0530

    corrected naming of css file

--- components/molecules/productList.js
+++ components/molecules/productList.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from './styles/productlist.module.scss';
+import styles from './styles/products.module.scss';
 
 const ProductList = ({ value }) => (
     <div className={styles.productitem}>
